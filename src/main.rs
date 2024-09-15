70f6b5ab5bfe6d18ae1686c8ad64208617617e9d
Marcos Montiel <marcosmontielrej@gmail.com>
2024-09-15 00:18:30 -0300

    Ratatui Hello World.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use crossterm::event::{self, KeyCode, KeyEventKind};
+use ratatui::{style::Stylize, widgets::Paragraph, DefaultTerminal};
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    terminal.clear()?;
+    let app_result = run(terminal);
+    ratatui::restore();
+    app_result
+}
+
+fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
+    loop {
+        terminal.draw(|frame| {
+            let greeting = Paragraph::new("Hello Ratatui! (pres 'q' ti quit)")
+                .white()
+                .on_blue();
+            frame.render_widget(greeting, frame.area());
+        })?;
+
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(());
+            }
+        }
+    }
 }
